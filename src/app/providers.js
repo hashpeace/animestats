609fa11a6e6727f30a1cdfe8a7ebb5b6121a4bfa
hashpeace <info@animestats.tf>
2026-03-08 17:15:48 +0100

    don't optimize images anymore

--- src/app/providers.js
+++ src/app/providers.js
@@ -5,6 +5,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined' && location.hostname !== "localhost") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    capture_pageview: 'history_change',
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
   })
 }
